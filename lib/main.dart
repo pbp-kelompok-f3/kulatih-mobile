c85151a459669aee7f70b4bc64d20e12812b4862
inoperatingsystem <julius.albert@cs.ui.ac.id>
2025-12-21 18:23:41 +0700

    Profile My Bookings and My Reviews Button udah bisa

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:kulatih_mobile/albert-user/screens/login.dart';
 import 'package:kulatih_mobile/models/user_provider.dart';
 import 'package:kulatih_mobile/navigationbar.dart';
 import 'package:kulatih_mobile/salman-tournament/page/tournament_main.dart';
-import 'package:kulatih_mobile/azizah-rating/screens/reviews_list_page.dart';
 import 'package:kulatih_mobile/alia-community/pages/community_page.dart';
 import 'package:kulatih_mobile/theme/app_colors.dart';
 import 'package:kulatih_mobile/albert-user/screens/find_coach.dart';
@@ -52,22 +51,26 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
   final String title;
+  final int initialIndex;
+
+  const MyHomePage({super.key, required this.title, this.initialIndex = 0});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
+  late int currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.initialIndex;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final request = context.watch<CookieRequest>();
-    final userProvider = context.watch<UserProvider>();
-    final profile = userProvider.userProfile;
-
     List<Widget> pages = [
       // ================= INDEX 0: FIND COACH (HOME) =================
       const FindCoach(),
@@ -93,52 +96,6 @@ class _MyHomePageState extends State<MyHomePage> {
         onTap: (index) {
           setState(() => currentIndex = index);
         },
-      ),
-    );
-  }
-
-  // ================= HELPER CARD =================
-  Widget _buildInfoCard({
-    required IconData icon,
-    required String label,
-    required String value,
-  }) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.05),
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: const Color(0xFFE8B923).withOpacity(0.3)),
-      ),
-      child: Row(
-        children: [
-          Icon(icon, color: const Color(0xFFE8B923), size: 24),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  label,
-                  style: const TextStyle(
-                    fontFamily: 'BeVietnamPro',
-                    fontSize: 12,
-                    color: Colors.white54,
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  value,
-                  style: const TextStyle(
-                    fontFamily: 'BeVietnamPro',
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
